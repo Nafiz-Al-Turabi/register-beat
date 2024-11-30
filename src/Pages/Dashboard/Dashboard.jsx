8ca0896c3882dd6d678fd0598bc4da332a31b620
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2024-11-30 16:57:11 +0600

    Dashboard updated

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,18 @@
 import React, { useState } from "react";
-import { FaMusic, FaCrown, FaCog, FaSignOutAlt, FaBars } from "react-icons/fa";
+import { FaCrown, FaCog, FaSignOutAlt, FaBars, FaPlus } from "react-icons/fa";
 import { FaHeadphonesSimple } from "react-icons/fa6";
-import { HiOutlineMusicalNote } from "react-icons/hi2";
-import { MdOutlineDashboard } from "react-icons/md";
-import { RiMusic2Line } from "react-icons/ri";
+import { GrLineChart } from "react-icons/gr";
+import { LuCreditCard } from "react-icons/lu";
+import { MdKeyboardArrowDown, MdOutlineDashboard } from "react-icons/md";
+import { RiMusic2Line, RiUser3Line } from "react-icons/ri";
 import { Outlet, Link } from "react-router-dom";
 
 const Dashboard = () => {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isDropdown, setDropdown] = useState(false)
+    const toggleDropdown = () => {
+        setDropdown(!isDropdown)
+    }
 
     const toggleSidebar = () => {
         setIsSidebarOpen(!isSidebarOpen);
@@ -20,21 +25,55 @@ const Dashboard = () => {
                 className={`fixed top-0 left-0 h-full bg-[#0f0f0f] w-64 p-4 flex flex-col transition-transform transform ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"
                     } md:translate-x-0 z-50`}
             >
-                <div className="flex items-center mb-8 border border-gray-700 bg-[#1e1e1e] p-3 rounded-md">
-                    <div className="bg-gray-800 w-10 h-10 rounded-md flex items-center justify-center text-xl">
-                        US
+                <div className="relative">
+                    <div onClick={toggleDropdown} className="flex items-center justify-between mb-8 border border-gray-700 bg-[#1e1e1e] p-3 rounded-md cursor-pointer">
+                        <div className="flex items-center ">
+                            <div className="bg-gray-800 w-10 h-10 rounded-md flex items-center justify-center text-xl">
+                                US
+                            </div>
+                            <div className="ml-3 ">
+                                <p className="text-sm">Usuario</p>
+                                <p className="text-xs text-gray-400">Plan Básico</p>
+                            </div>
+                        </div>
+                        <span
+                            className={`transform transition-transform duration-300 ${isDropdown ? "rotate-180" : "rotate-0"
+                                }`}
+                        >
+                            <MdKeyboardArrowDown className="text-2xl" />
+                        </span>
                     </div>
-                    <div className="ml-3 ">
-                        <p className="text-sm">Usuario</p>
-                        <p className="text-xs text-gray-400">Plan Básico</p>
-                    </div>
+                    {
+                        isDropdown && (
+                            <div className="absolute space-y-5 -bottom-32 text-white bg-[#1e1e1e] p-4 w-full border border-gray-700 rounded-md animate-dropdown ">
+                                <Link
+                                    to="/dashboard"
+                                    className="flex items-center text-base text-gray-300 hover:text-white"
+                                >
+                                    <RiUser3Line className="mr-2" /> Edit Profile
+                                </Link>
+                                <Link
+                                    to="/dashboard"
+                                    className="flex items-center text-base text-gray-300 hover:text-white"
+                                >
+                                    <LuCreditCard className="mr-2" /> Manage Subscription
+                                </Link>
+                                <Link
+                                    to="/dashboard"
+                                    className="flex items-center text-base text-gray-300 hover:text-white"
+                                >
+                                    <GrLineChart className="mr-2" /> Upgrade you Plan
+                                </Link>
+                            </div>
+                        )
+                    }
                 </div>
                 <nav className="space-y-6">
                     <Link
                         to="/dashboard"
                         className="flex items-center text-lg text-gray-300 hover:text-white"
                     >
-                        <MdOutlineDashboard className="mr-2"/> Dashboard
+                        <MdOutlineDashboard className="mr-2" /> Dashboard
                     </Link>
                     <Link
                         to="/dashboard/my-beats"
@@ -78,14 +117,17 @@ const Dashboard = () => {
 
             {/* Main Content */}
             <div className="flex-1 md:ml-64">
-                <div className="flex items-center justify-between bg-black p-4 md:pl-8">
+                <div className="flex items-center justify-between bg-[#0f0f0f] p-4 md:pl-8">
                     <button
                         className="text-white md:hidden"
                         onClick={toggleSidebar}
                     >
                         <FaBars className="text-2xl" />
                     </button>
-                    <h1 className="text-purple-500 text-2xl">Dashboard</h1>
+                    <div>
+                        <input type="text" className="py-1.5 px-5 w-96 rounded-full outline-none bg-transparent text-gray-200  border-[0.5px] border-violet-400 " placeholder="Search your beat..." />
+                    </div>
+                    <button className="flex items-center text-lg font-bold secondary-bg py-1.5 px-5 rounded-full hover:bg-violet-700 duration-300 active:scale-95 ">Register a Beat <FaPlus className="ml-2 text-xl border p-0.5 rounded-sm " /></button>
                 </div>
 
                 {/* Content Section */}
