478d83b7bba84112741ebca9301b43fdab9e417b
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2025-01-16 12:42:18 +0600

    transanction table data added

--- src/Components/Tables/TransactionsTable.jsx
+++ src/Components/Tables/TransactionsTable.jsx
@@ -1,40 +1,42 @@
 import React from 'react';
 
-const TransactionsTable = () => {
+const TransactionsTable = ({ transactions }) => {
     return (
-        <tr
-            key={transaction._id}
-            className="hover:bg-zinc-800 even:bg-[#212529] transition-colors"
-        >
-            <td className="px-4 py-4 text-white">
-                {transaction._id}
-            </td>
-            <td className="px-4 py-4 text-white">
-                {transaction.userNAme || ''}
-            </td>
-            <td className="px-4 py-4 text-zinc-300">
-                {transaction.userEmail || ''}
-            </td>
-            <td className="px-4 py-4 text-zinc-300">
-                {transaction.credit}
-            </td>
-            <td className="px-4 py-4 text-zinc-300">
-                {transaction.customerId}
-            </td>
-            <td
-                className={`px-4 py-4 font-bold ${getMethodColor(
-                    transaction.method
-                )}`}
-            >
-                {transaction.method}
-            </td>
-            <td className="px-4 py-4 text-zinc-300">
-                {transaction.amount}$
-            </td>
-            <td className="px-4 py-4 text-zinc-300">
-                {new Date(transaction.createdAt).toLocaleString()}
-            </td>
-        </tr>
+        <>
+            {
+                transactions.map((transaction) => (<tr
+                    key={transaction._id}
+                    className="hover:bg-zinc-800 even:bg-[#212529] transition-colors"
+                >
+                    <td className="px-4 py-4 text-white">
+                        {transaction._id}
+                    </td>
+                    <td className="px-4 py-4 text-white">
+                        {transaction.userNAme || ''}
+                    </td>
+                    <td className="px-4 py-4 text-zinc-300">
+                        {transaction.userEmail || ''}
+                    </td>
+                    <td className="px-4 py-4 text-zinc-300">
+                        {transaction.credit}
+                    </td>
+                    <td className="px-4 py-4 text-zinc-300">
+                        {transaction.customerId}
+                    </td>
+                    <td
+                        className={`px-4 py-4 font-bold `}
+                    >
+                        {transaction.method}
+                    </td>
+                    <td className="px-4 py-4 text-zinc-300">
+                        {transaction.amount}$
+                    </td>
+                    <td className="px-4 py-4 text-zinc-300">
+                        {new Date(transaction.createdAt).toLocaleString()}
+                    </td>
+                </tr>))
+            }
+        </>
     );
 };
 
