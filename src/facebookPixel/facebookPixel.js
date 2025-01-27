77983ae1dab8dc0825991435715624dd3d2a1895
Nafiz Al Turabi <nafizalturabi18cse@gmail.com>
2025-01-27 12:55:16 +0600

    facebookpixel

--- src/facebookPixel/facebookPixel.js
+++ src/facebookPixel/facebookPixel.js
@@ -2,14 +2,11 @@ import ReactPixel from "react-facebook-pixel";
 
 export const initFacebookPixel = () => {
   ReactPixel.init("1391827555520798");
-  ReactPixel.pageView({
-    path: window.location.pathname
-  }); 
+  ReactPixel.pageView(); 
 };
 
 export const trackEvent = (eventName) => {
   ReactPixel.track(eventName);
-  // Track page views with route name
   ReactPixel.track('PageView', {
     path: window.location.pathname
   });
