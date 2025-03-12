f562a9dc06505d0537b7c5577846b55d4af8804a
Tohidul0 <tohidulalam967418@gmail.com>
2025-03-12 08:42:47 +0600

    vps url add

--- src/Axios/AxiosInstance.jsx
+++ src/Axios/AxiosInstance.jsx
@@ -2,8 +2,10 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3001/api',
-    //baseURL: 'https://louizmizik-server.onrender.com/api',
+    // baseURL: 'http://localhost:3001/api',
+    baseURL: 'http://backend.beatprotect.io/api',
+     //baseURL: 'https://louizmizik-server.onrender.com/api',
+    //baseURL: 'https://phpstack-1408986-5240724.cloudwaysapps.com/api',
     withCredentials: true,
 });
 
