edc18ad6a8b88bbb2ff9382e72d8ef7fa89db3c4
Tohidul0 <tohidulalam967418@gmail.com>
2025-03-12 12:44:00 +0600

    add ssl link for vps

--- src/Axios/AxiosInstance.jsx
+++ src/Axios/AxiosInstance.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:3001/api',
-    baseURL: 'http://backend.beatprotect.io/api',
+    baseURL: 'https://backend.beatprotect.io/api',
      //baseURL: 'https://louizmizik-server.onrender.com/api',
     //baseURL: 'https://phpstack-1408986-5240724.cloudwaysapps.com/api',
     withCredentials: true,
