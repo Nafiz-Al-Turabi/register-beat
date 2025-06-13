d5f1b2efd50ec1228f383b292c2c460c19d030d9
Nafiz-Al-Turabi <nafizalturabi18cse@gmail.com>
2025-06-13 12:36:08 +0600

    updated

--- src/Layout/landingLayout.jsx
+++ src/Layout/landingLayout.jsx
@@ -3,9 +3,11 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../Shared/Navbar';
 import Footer from '../Shared/Footer';
 import CookieBar from '../Components/CookieBar/CookieBar';
+import MetaPixelTracker from '../facebookPixel/MetaPixelTracker';
 const LandingLayout = () => {
     return (
         <div>
+            <MetaPixelTracker />
             <Navbar />
             <Outlet />
             <Footer />
